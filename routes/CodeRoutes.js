e262c8b9f26eb8b620b57905b084732a1dd3515c
mdnelles <mxnelles@gmail.com>
2020-05-10 14:32:49 -0400

    update

---
+++ routes/CodeRoutes.js
@@ -0,0 +1,34 @@
+const express = require("express"),
+   code = express.Router(),
+   Sequelize = require("sequelize"),
+   db = require("../database/db"),
+   cors = require("cors"),
+   c = require("../config/config.json"),
+   Logfn = require("../components/Logger"),
+   rf = require("./RoutFuctions");
+
+code.use(cors());
+
+code.post("/get_tables", rf.verifyToken, (req, res) => {
+   db.sequelize
+      .query("SHOW TABLES")
+      .then((data) => {
+         res.json({ success: true, data: data });
+      })
+      .catch((err) => {
+         console.log("Err CodeRoutes.get_tables" + err);
+         Logfn.log2db(
+            500,
+            fileName,
+            "could SHOW TABLES",
+            "catch err",
+            err,
+            ip,
+            req.headers.referer,
+            tdate
+         );
+         res.json({ success: false, data: "na", err: err });
+      });
+});
+
+module.exports = code;
--- server.js
+++ server.js
@@ -47,11 +47,13 @@ app.use(
 var Import = require("./routes/ImportRoutes.js"),
    Captcha = require("./routes/CaptchaRoutes"),
    Lakes = require("./routes/LakesRoutes"),
+   Code = require("./routes/CodeRoutes"),
    User = require("./routes/UserRoutes");
 
 app.use("/import", Import);
 app.use("/lakes", Lakes);
 app.use("/user", User);
+app.use("/code", Code);
 app.use("/captcha", Captcha);
 
 // serve static assets if in production
