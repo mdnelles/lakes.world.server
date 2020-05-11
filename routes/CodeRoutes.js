78cfbbb739399845ad0d9827d0aa5ff04e81999d
mdnelles <mxnelles@gmail.com>
2020-05-11 11:54:33 -0400

    update

--- routes/CodeRoutes.js
+++ routes/CodeRoutes.js
@@ -31,4 +31,26 @@ code.post("/get_tables", rf.verifyToken, (req, res) => {
       });
 });
 
+code.post("/get_columns", rf.verifyToken, (req, res) => {
+   db.sequelize
+      .query("SHOW COLUMNS FROM " + req.body.table)
+      .then((data) => {
+         res.json({ success: true, data: data });
+      })
+      .catch((err) => {
+         console.log("Err CodeRoutes.get_tables" + err);
+         Logfn.log2db(
+            500,
+            fileName,
+            "could SHOW COLUMNS",
+            "catch err",
+            err,
+            ip,
+            req.headers.referer,
+            tdate
+         );
+         res.json({ success: false, data: "na", err: err });
+      });
+});
+
 module.exports = code;
